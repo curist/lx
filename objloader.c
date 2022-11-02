d79c8c5b49a0583cff52a92c59975e9a336ed1e3
curist <curist.cyc@gmail.com>
2022-11-02 16:57:36 +0800

    let objloader read constant section

--- objloader.c
+++ objloader.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <string.h>
 
 #include "common.h"
 #include "objloader.h"
 #include "chunk.h"
+
 
 // obj layout
 // LXOBJ:     5
@@ -10,8 +12,24 @@
 // OBJSIZE:   4 little endian
 // TBD:       32 - 10 = 22
 // CODE_SECTION: ?
+//      CONST_COUNT: 1
 //      SIZE: 4 little endian
+// CONST_SECTION: follow right after a CODE_SECTION (XXX: or should we do some padding?)
+//      SIZE: 4
+//   every const is like
+//      TYPE:  1
+//      VALUE: various length
+//      currently we only have TYPE 00, as double, and it takes 8 bytes
 // constants section should be defined at TBD: later
+
+
+double readDouble(const uint8_t* bytes) {
+  double n = 0;
+  memcpy(&n, bytes, sizeof(double));
+
+  return n;
+}
+
 bool loadObj(const uint8_t* bytes, Chunk* chunk) {
   if (!(bytes[0] == 'L' && bytes[1] == 'X' &&
         bytes[2] == 'O' && bytes[3] == 'B' && bytes[4] == 'J')) {
@@ -31,19 +49,29 @@ bool loadObj(const uint8_t* bytes, Chunk* chunk) {
   obj_size += code[9] << 24;
 
   size_t code_size = 0;
-  code_size += code[32];
-  code_size += code[33] << 8;
-  code_size += code[34] << 16;
-  code_size += code[35] << 24;
+  code_size += code[33];
+  code_size += code[34] << 8;
+  code_size += code[35] << 16;
+  code_size += code[36] << 24;
 
   // XXX: naive size check, only works for 1 code chunk/section
-  if (code_size > obj_size - 36) {
+  if (code_size > obj_size - 37) {
     fprintf(stderr, "Code size larger than obj size.\n");
     return false;
   }
 
   for (int i = 0; i < code_size; i++) {
-    writeChunk(chunk, code[36 + i], 1);
+    writeChunk(chunk, code[37 + i], 1);
+  }
+
+  uint8_t* constSection = &code[37 + code_size];
+  for (int i = 0; i < code[32]; i++) {
+    // we are going to ^ read this many constants
+    uint8_t type = constSection[0];
+    // XXX  ^ should be 0 atm, which means double
+    double value = readDouble(&constSection[1]);
+    constSection += (1 + 8); // type + double 8 bytes
+    addConstant(chunk, value);
   }
 
   return true;

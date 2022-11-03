b459ddb107887e78fb461efecf030f8caccb16d8
curist <curist.cyc@gmail.com>
2022-11-03 13:07:52 +0800

    ch 19.4.0

--- object.c
+++ object.c
@@ -28,3 +28,11 @@ ObjString* copyString(const char* chars, int length) {
   heapChars[length] = '\0';
   return allocateString(heapChars, length);
 }
+
+void printObject(Value value) {
+  switch (OBJ_TYPE(value)) {
+    case OBJ_STRING:
+      printf("%s", AS_CSTRING(value));
+      break;
+  }
+}

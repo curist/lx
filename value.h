7bebd421135fbb0a1592a773db7aeb2f84f810f2
curist <curist.cyc@gmail.com>
2022-11-03 09:14:53 +0800

    WIP: ch 18 string

--- value.h
+++ value.h
@@ -3,11 +3,14 @@
 
 #include "common.h"
 
+typedef struct Obj Obj;
+typedef struct ObjString ObjString;
 
 typedef enum {
   VAL_BOOL,
   VAL_NIL,
   VAL_NUMBER,
+  VAL_OBJ,
 } ValueType;
 
 typedef struct {
@@ -15,6 +18,7 @@ typedef struct {
   union {
     bool boolean;
     double number;
+    Obj* obj;
   } as;
 } Value;
 
@@ -25,9 +29,11 @@ typedef struct {
 } ValueArray;
 
 
+#define AS_OBJ(value)     ((value).as.obj)
 #define IS_BOOL(value)    ((value).type == VAL_BOOL)
 #define IS_NIL(value)     ((value).type == VAL_NIL)
 #define IS_NUMBER(value)  ((value).type == VAL_NUMBER)
+#define IS_OBJ(value)     ((value).type == VAL_OBJ)
 
 #define AS_BOOL(value)    ((value).as.boolean)
 #define AS_NUMBER(value)  ((value).as.number)
@@ -35,6 +41,7 @@ typedef struct {
 #define BOOL_VAL(value)   ((Value){VAL_BOOL, {.boolean = value}})
 #define NIL_VAL           ((Value){VAL_NIL, {.number = 0}})
 #define NUMBER_VAL(value) ((Value){VAL_NUMBER, {.number = value}})
+#define OBJ_VAL(object)   ((Value){VAL_OBJ, {.obj = (Obj*)object}})
 
 bool valuesEqual(Value a, Value b);
 void initValueArray(ValueArray* array);

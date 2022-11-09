a3b954ff8b5598cd86372047b49d76506363f4e8
curist <curist.cyc@gmail.com>
2022-11-09 15:58:13 +0800

    update comment and fix objIsValid

--- objloader.c
+++ objloader.c
@@ -6,11 +6,7 @@
 #include "objloader.h"
 #include "object.h"
 
-ObjFunction* currentFunction = NULL;
-
-static Chunk* currentChunk() {
-  return &currentFunction->chunk;
-}
+ValueArray functions;
 
 // obj layout
 // LX:        2
@@ -36,17 +32,14 @@ static Chunk* currentChunk() {
 //        NIL:    1
 //        NUMBER: 1 + 8 (double)
 //        OBJ:    1 + 1 (obj type) + obj type dependent layout(length)
-//          STRING: 4 little endian size + actual string
+//          STRING:   4 little endian size + actual string
+//          FUNCTION: 0, we rely on the build order to stay the same
 // DEBUG_SECTION:
 //      SIZE: 4 little endian
 //      FILEPATH_LENGTH: 2 file path length
 //      FILEPATH: vary length
 //      TOKEN_LINE_NUMBER: 2 bytes each (which means we would only support line no. up to 65535)
 
-// NOTE: we should use TBD to store a chunks jump table address
-// and chunks jump table will have layout like
-// CHUNKS: 2 little endian
-//   ADDR(s): 4 little endian, each
 
 double readDouble(const uint8_t* bytes) {
   double n = 0;
@@ -106,7 +99,8 @@ bool objIsValid(uint8_t* bytes) {
     ptr += 4;
 
     if (total_size + 4 + chunk_size > obj_size) {
-      fprintf(stderr, "Invalid lxobj: chunk %d size is too big.\n", i);
+      // ensure this chunk won't exceed total object size
+      fprintf(stderr, "Invalid lxobj: chunk %d size is too big(%ld).\n", i, chunk_size);
       return false;
     }
 
@@ -114,48 +108,52 @@ bool objIsValid(uint8_t* bytes) {
     chunk_size_sofar += code_size;
     ptr += 4 + code_size;
     if (chunk_size_sofar > chunk_size) {
-      fprintf(stderr, "Invalid lxobj: chunk %d code size is too big.\n", i);
+      fprintf(stderr, "Invalid lxobj: chunk %d code size is too big(%ld).\n", i, code_size);
       return false;
     }
 
     size_t constant_size = getSize(ptr);
     chunk_size_sofar += 4 + constant_size;
-    ptr += constant_size;
+    ptr += 4 + constant_size;
     if (chunk_size_sofar > chunk_size) {
-      fprintf(stderr, "Invalid lxobj: chunk %d constants size is too big.\n", i);
+      fprintf(stderr, "Invalid lxobj: chunk %d constants size is too big(%ld).\n", i, constant_size);
       return false;
     }
 
     if (debug) {
       size_t debug_section_size = getSize(ptr);
       chunk_size_sofar += 4 + debug_section_size;
-      ptr += debug_section_size;
+      ptr += 4 + debug_section_size;
       if (chunk_size_sofar > chunk_size) {
-        fprintf(stderr, "Invalid lxobj: chunk %d debug section size is too big.\n", i);
+        fprintf(stderr, "Invalid lxobj: chunk %d debug section size is too big(%ld).\n", i, debug_section_size);
         return false;
       }
     }
 
     if (chunk_size != chunk_size_sofar) {
       fprintf(stderr, "Invalid lxobj: chunk %d size mismatch.\n", i);
+      return false;
     }
 
     // all good, set chunk_start to next chunk
-    chunk_start += chunk_size;
-    total_size += chunk_size;
+    chunk_start += 4 + chunk_size;
+    total_size += 4 + chunk_size;
   }
 
   return true;
 }
 
-bool loadChunk(uint8_t* bytes, uint8_t flags) {
+ObjFunction* loadChunk(uint8_t* bytes, uint8_t flags) {
   bool debug = (flags & 0b00000001) > 0;
 
   size_t code_size = getSize(&bytes[4]);
 
+  ObjFunction* func = newFunction();
+  Chunk* chunk = &func->chunk;
+
   if (!debug) {
     for (int i = 0; i < code_size; i++) {
-      writeChunk(currentChunk(), bytes[4 + 4 + i], 1);
+      writeChunk(chunk, bytes[4 + 4 + i], 1);
     }
   } else {
     // to gather debug line info, we must fastforward
@@ -174,7 +172,7 @@ bool loadChunk(uint8_t* bytes, uint8_t flags) {
 
     for (int i = 0; i < code_size; i++) {
       uint16_t line = getShortSize(&ptr[i * 2]);
-      writeChunk(currentChunk(), bytes[4 + 4 + i], line);
+      writeChunk(chunk, bytes[4 + 4 + i], line);
     }
   }
 
@@ -191,90 +189,92 @@ bool loadChunk(uint8_t* bytes, uint8_t flags) {
       case VAL_BOOL:
         // probabaly don't need this
         // since bool is encoded in bytecode already
-        addConstant(currentChunk(), BOOL_VAL(constSection[1]));
+        addConstant(chunk, BOOL_VAL(constSection[1]));
         constSection += (1 + 1);
         break;
 
       case VAL_NIL:
         // probabaly don't need this
         // since nil is encoded in bytecode already
-        addConstant(currentChunk(), NIL_VAL);
+        addConstant(chunk, NIL_VAL);
         constSection += 1;
         break;
 
       case VAL_NUMBER:
-        addConstant(currentChunk(), NUMBER_VAL(readDouble(&constSection[1])));
+        addConstant(chunk, NUMBER_VAL(readDouble(&constSection[1])));
         constSection += (1 + 8); // type + double 8 bytes
         break;
 
       case VAL_OBJ: {
         ObjType objType = constSection[1];
         switch (objType) {
+          case OBJ_FUNCTION: {
+            // TODO: add nil constant, & save the value pointer to an array
+            // after all chunks(functions) are loaded and saved to global functions var,
+            // we will iterate through value pointer,
+            // and do valuePointer[i] = OBJ_VAL(functions[i])
+            break;
+          }
           case OBJ_STRING: {
             size_t ssize = getSize(&constSection[2]);
-            addConstant(currentChunk(),
+            addConstant(chunk,
                 OBJ_VAL(copyString((char*)&constSection[6], ssize)));
             // value type + obj type + 4(ssize) + actual string
             constSection += (1 + 1 + 4 + ssize);
             break;
           }
-          // TODO: OBJ_FUNCTION
-          // store addConstant index
-          // then scan chunk and compose function
-          // then set currentfunction.chunk.constants[index]
-          // NOTE: function chunks loading should be recursive
-          // chunks could contains other function chunk
-          // but if we load chunks using the same order we write chunks,
-          // we might have less trouble?
-          // NOTE: use clox ARRAY to store function.chunk.constants pointer
-          // and its correspond chunk number
-          // then when we finished loading chunks (into another clox ARRAY)
-          // we can go through first array, and replace the function value in place
           default:
             fprintf(stderr, "Invalid object type %x\n", objType);
-            return false;
+            return NULL;
         }
         break;
       }
 
       default:
         fprintf(stderr, "Invalid value type %x\n", type);
-        return false;
+        return NULL;
     }
   }
-  return true;
+  return func;
 }
 
 ObjFunction* loadObj(uint8_t* bytes) {
   if (!objIsValid(bytes)) {
     return NULL;
   }
-
-  // XXX: we are hanlding only the first code section for now
-  currentFunction = newFunction();
-
-  // ObjFunction* func = newFunction();
-  // ObjFunction* enclosing = currentFunction;
-  // currentFunction = func;
-  // currentFunction = enclosing;
+  ObjFunction* main = NULL;
+  initValueArray(&functions);
 
   uint8_t flags = bytes[3];
   bool debug = (flags & 0b00000001) > 0;
 
   uint16_t chunks_count = getShortSize(&bytes[8]);
   uint8_t* chunk_start = &bytes[16];
-  printf("chunks count: %d\n", chunks_count);
 
   for (int i = 0; i < chunks_count; i++) {
     size_t chunk_size = getSize(chunk_start);
-    if (!loadChunk(chunk_start, flags)) return NULL;
+    ObjFunction* func = loadChunk(chunk_start, flags);
+    if (func == NULL) return NULL;
+    if (i == 0) main = func;
+    writeValueArray(&functions, OBJ_VAL(func));
     chunk_start += chunk_size;
   }
 
-#ifdef DEBUG_PRINT_CODE
-  disassembleChunk(currentChunk(), currentFunction->name != NULL
-      ? currentFunction->name->chars : "<script>");
-#endif
+  // NOTE: write function at value index
+  // ObjFunction* xx = AS_FUNCTION(functions.values[0]);
+  // int count = xx->chunk.constants.count;
+  // xx->chunk.constants.values[0] = OBJ_VAL(main);
+  // for (int i = 0; i < count; i++) {
+  //   Value* v = xx->chunk.constants.values;
+  //   printValue(v[i]);
+  //   printf("\n");
+  // }
+  // printValue(functions.values[0]);
 
-  return currentFunction;
+#ifdef DEBUG_PRINT_CODE
+  disassembleChunk(&main->chunk, main->name != NULL
+      ? main->name->chars : "<script>");
+#endif
+  freeValueArray(&functions);
+  return main;
 }

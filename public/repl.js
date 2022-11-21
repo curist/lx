2301421e97da05d7e929cbfc9d997aa2018f9f11
curist <curist.cyc@gmail.com>
2022-11-21 16:00:52 +0800

    cmd styling

--- public/repl.js
+++ public/repl.js
@@ -1,17 +1,23 @@
-const print = (function() {
-  var output = document.getElementById('output')
+const output = document.getElementById('output')
 
-  return function(...args) {
-    const text = args.join(' ')
-    output.innerText += text + '\n'
-    output.scrollTop = output.scrollHeight
-  };
-})()
+function appendLine(line, className = '') {
+  const div = document.createElement('div')
+  div.className = className
+  div.textContent = line
+  output.appendChild(div)
+  output.scrollTop = output.scrollHeight
+}
+
 var Module = {
-  preRun: [],
   postRun: [],
-  print,
-  printErr: print,
+  print(...args) {
+    const text = args.join(' ')
+    appendLine(text)
+  },
+  printErr(...args) {
+    const text = args.join(' ')
+    appendLine(text, 'err')
+  },
   setStatus(text) { console.log('set status:', text) },
   totalDependencies: 0,
   monitorRunDependencies: function(left) {
@@ -19,7 +25,6 @@ var Module = {
     Module.setStatus(left ? 'Preparing... (' + (this.totalDependencies-left) + '/' + this.totalDependencies + ')' : 'All downloads complete.');
   }
 };
-Module.setStatus('Downloading...');
 window.onerror = function() {
   Module.setStatus('Exception thrown, see JavaScript console');
   Module.setStatus = function(text) {
@@ -33,6 +38,7 @@ const prompters = []
 replInputForm.addEventListener('submit', e => {
   e.preventDefault()
   const line = replInput.value
+  appendLine(line, 'lxcmd')
   replInput.value = ''
   const prompter = prompters.shift()
   if (prompter) { prompter(line) }
@@ -41,6 +47,14 @@ replInputForm.addEventListener('submit', e => {
 window.getLineInput = function() {
   return new Promise(resolve => prompters.push(resolve))
 }
+
+document.body.addEventListener('click', e => {
+  if(e.target.className == 'lxcmd') {
+    replInput.value = e.target.textContent
+    setTimeout(() => replInput.focus())
+    return
+  }
+})
 
 import createLxModule from './lx.js'
 const lx = await createLxModule(Module)

2841974d11656449d0496bce417db243fd991591
curist <curist.cyc@gmail.com>
2023-11-14 07:55:14 +0800

    minor tweak disasm closure upvalues format

--- src/debug.c
+++ src/debug.c
@@ -154,8 +154,8 @@ int disassembleInstruction(Chunk* chunk, int offset, bool printCode) {
       for (int j = 0; j < function->upvalueCount; j++) {
         int isLocal = chunk->code[offset++];
         int index = chunk->code[offset++];
-        printf("%04d      |                     %s %d\n",
-               offset - 2, isLocal ? "local" : "upvalue", index);
+        printf("%04d      |                       %s %d\n",
+               offset - 2, isLocal ? "local  " : "upvalue", index);
       }
 
       return offset;
